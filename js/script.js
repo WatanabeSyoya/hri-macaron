2b7500fb0856673e5b14d104b66d343c129ea96d
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-05-12 08:19:01 +0900

    ハンバーガー

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.menu-trigger').click(function () { //メニューボタンタップ後の処理
+    $(this).toggleClass('active'); //クリックした要素に「.active」要素を付与
+    $('.gnavi').css('display', 'block');//「.gnavi」要素の非表示を表示する
+    if ($(this).hasClass('active')) { //もしクリックした要素に「.active」要素があれば
+      $('.gnavi').addClass('active');　 //「.active」要素を付与
+    } else {                            //「.active」要素が無ければ
+      $('.gnavi').removeClass('active'); //「.active」要素を外す
+    }
+  });
+});
