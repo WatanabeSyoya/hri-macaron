4da130649523f9fcb34fa2b8db9780bd46b95e0f
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-05-12 12:36:18 +0900

    スライド機能

--- js/script.js
+++ js/script.js
@@ -9,3 +9,30 @@ $(function () {
     }
   });
 });
+
+$(function () {
+  function toggleChangeBtn() {
+    var slideIndex = $('.staff-box').index($('.active'));
+    $('.button').show();
+    if (slideIndex == 0) {
+      $('.prev').hide();
+    } else if (slideIndex == 3) {
+      $('.next').hide();
+    }
+  }
+
+  toggleChangeBtn();
+
+  $('.next').click(function () {/*nextボタンを押したとき*/
+    var $displaySlide = $('.active');/*現在表示中のスライドを取得*/
+    $displaySlide.removeClass('active box-design');/*そのスライドからactiveクラスを除いて表示されないようにする。*/
+    $displaySlide.next().addClass('active box-design');/*次のスライドにactiveクラスをつけ、表示させる。*/
+    toggleChangeBtn();/*nextボタンを隠すか判断*/
+  });
+  $('.prev').click(function () {/*prevボタンを押したとき*/
+    var $displaySlide = $('.active');/*現在表示中のスライドを取得*/
+    $displaySlide.removeClass('active box-design');/*そのスライドからactiveクラスを除いて表示されないようにする。*/
+    $displaySlide.prev().addClass('active box-design');/*前のスライドにactiveクラスをつけ、表示させる。*/
+    toggleChangeBtn();/*prevボタンを隠すか判断*/
+  });
+});
